b31ebc133f7f34abc27e466c80fadda04ace4129
DESKTOP-JURDD5A\ONLINE-DEV02 <ghtn9398@nate.com>
2023-05-16 10:09:15 +0900

    스프링 커스텀 필터 테스트

---
+++ src/main/java/com/cos/jwt/filter/MyFilter2.java
@@ -0,0 +1,16 @@
+package com.cos.jwt.filter;
+
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class MyFilter2 implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+        System.out.println("필터2");
+
+        chain.doFilter(request,response); //필터 체인을 다시 타도록 등록. 여기서 말하는 필터체인은 스프링 시큐리티 필터체인
+    }
+}
