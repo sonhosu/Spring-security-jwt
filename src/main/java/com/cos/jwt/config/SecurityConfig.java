01c1b127645b0e82243dac908db2f3f2aa2b529b
DESKTOP-JURDD5A\ONLINE-DEV02 <ghtn9398@nate.com>
2023-05-16 15:38:03 +0900

    JwtAuthenticationFilter 커스텀필터 추가

--- src/main/java/com/cos/jwt/config/SecurityConfig.java
+++ src/main/java/com/cos/jwt/config/SecurityConfig.java
@@ -1,9 +1,12 @@
 package com.cos.jwt.config;
 
+import com.cos.jwt.JwtAuthenticationFilter;
 import com.cos.jwt.filter.MyFilter1;
-import com.cos.jwt.filter.MyFilter3;
+//import com.cos.jwt.filter.MyFilter3;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -19,18 +22,22 @@ import org.springframework.web.filter.CorsFilter;
 @RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
     private final CorsFilter corsFilter;
+    @Autowired
+    private final CorsConfig corsConfig;
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         //스프링 시큐리티 필터가 실행되기전에 MyFilter1 가 실행되도록 등록. .addFilter() 이렇게 해서 바로 걸수없다.
-         http.addFilterBefore(new MyFilter3(), SecurityContextPersistenceFilter.class);
+        // http.addFilterBefore(new MyFilter3(), SecurityContextPersistenceFilter.class);
         http.csrf().disable();
          //세션을 사용하지 않겠다는 선언
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
               .and()
               .addFilter(corsFilter) //cross origin  정책을 벗어날수있다.
               .formLogin().disable()
-              .httpBasic().disable() //기본인증방식 제거
+              .httpBasic().disable() //기본인증방식 제거.
+                .addFilter(new JwtAuthenticationFilter(authenticationManager())) //JwtAuthenticationFilter 필터가 동작하도록 추가.
               .authorizeRequests()
               .antMatchers("/api/v1/user/**")
               .access("hasRole('ROLE_USER') or hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
